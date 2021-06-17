69aa34236a528053c609648f784dee6665373364
Arie May Wibowo <ariemayw@gmail.com>
2021-06-18 02:50:02 +0700

    Initial Commit

---
+++ travelmay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travelmay
+//
+//  Created by Arie May Wibowo on 18/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
